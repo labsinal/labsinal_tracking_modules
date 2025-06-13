b428f50d88ac93d2509984669c65f3dccc4d1d53
fred25 <fredericomattos25@gmail.com>
2025-06-13 09:52:02 -0300

    complete add_fate

--- ultrack_modules/misc/add_fate_tracking.py
+++ ultrack_modules/misc/add_fate_tracking.py
@@ -27,15 +27,15 @@ def add_fate_to_ultrack_table(ultrack_table : DataFrame) -> DataFrame:
     # get parent_ids
     parent_ids = ultrack_table[ultrack_table["track_id"].isin(ultrack_table["parent_track_id"])].track_id.unique()
 
-    last_frame_ids = new_table[new_table["t"] == max_frame].track_ids.unique()
+    last_frame_ids = new_table[new_table["t"] == max_frame].track_id.unique()
 
     for id in ultrack_table.track_id.unique():
         if id in parent_ids:
-            new_table[new_table.track_id == id]["fate"] = "mitosis"
+            new_table.loc[new_table.track_id == id, "fate"] = "mitosis"
         elif id in last_frame_ids:
-            new_table[new_table.track_id == id]["fate"] = "lived"
+            new_table.loc[new_table.track_id == id, "fate"] = "lived"
         else:
-            new_table[new_table.track_id == id]["fate"] = "death"
+            new_table.loc[new_table.track_id == id, "fate"] = "death"
 
     return new_table
 
@@ -55,14 +55,23 @@ def main() -> None:
                         required=True,
                         dest="input",
                         help="Ultrack table (.csv)")
-    
+   
+    parser.add_argument("-o", "--output",
+                        action="store",
+                        dest="output",
+                        required=False,
+                        default="",
+                        help="Output path (optional), if not set will overwrite original file")
+
     args_dict = vars(parser.parse_args())
 
     input_df = read_csv(args_dict["input"])
 
     output_df = add_fate_to_ultrack_table(input_df)
 
-    print(output_df)
+    save_path = args_dict["output"] if args_dict["output"] else args_dict["input"]
+    
+    output_df.to_csv(save_path, index = False)
 
 
 ######################################
